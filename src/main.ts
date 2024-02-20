4f92ede481ff113693ae48128f6fd20fb3bd3909
Matailin <mtailin@foxmail.com>
2024-02-20 17:01:59 +0800

    feat: 组件全局引入

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import { Tabbar,TabbarItem } from 'vant'
+
 import 'vant/lib/index.css'
 
 const app = createApp(App)
@@ -12,6 +15,9 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(Tabbar)
+app.use(TabbarItem)
+ 
 const rootValue = 16
 const rootWidth = 390
 const deviceWidth = document.documentElement.clientWidth
