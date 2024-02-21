df178ea462d8f93df5cb0d2b1c6f31df8ac0248f
Matailin <mtailin@foxmail.com>
2024-02-21 09:43:41 +0800

    feat: 自定义搜索组件

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { Tabbar,TabbarItem,Search } from 'vant'
+import { Tabbar,TabbarItem,Search,Icon } from 'vant'
 
 import 'vant/lib/index.css'
 
@@ -18,6 +18,7 @@ app.use(router)
 app.use(Tabbar)
 app.use(TabbarItem)
 app.use(Search)
+app.use(Icon)
  
 const rootValue = 16
 const rootWidth = 390
--- src/views/tabs/home/components/TheTop.vue
+++ src/views/tabs/home/components/TheTop.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import type {ISearchRecomment} from "@/types";
+import OpSearch from "@/components/OpSearch.vue";
+import {ref} from "vue";
 
 interface IProps {
-  recomments:ISearchRecomment[]
+  recomments: ISearchRecomment[]
 }
 
 defineProps<IProps>()
+
+const searchValue = ref("test")
+
+const onSearch = (v?: string|number)=>{
+  console.log('==== search', v);
+}
+const onCancel = ()=>{
+  console.log("==== cancel");
+}
+const onClear = ()=>{
+  console.log('==== clear')
+}
 </script>
 
 <template>
@@ -18,15 +32,12 @@ defineProps<IProps>()
 
     </div>
 
-    <VanSearch shape="round" background="linear-gradient(to right, rgb(53, 200, 250), rgb(31, 175, 243))"
-               placeholder="世界茶饮 35减2">
-      <template #right-icon>
-        <div>搜索</div>
-      </template>
-    </VanSearch>
+    <op-search v-model="searchValue" shape="round" background="linear-gradient(to right, rgb(53, 200, 250), rgb(31, 175, 243))"
+               placeholder="世界茶饮 35减2" show-action @search="onSearch" @cancel="onCancel" @clear="onClear">
+    </op-search>
 
     <div class="search-recommend">
-      <div v-for="v in recomments" :key="v.value" class="tag">{{v.label}}</div>
+      <div v-for="v in recomments" :key="v.value" class="tag">{{ v.label }}</div>
     </div>
   </div>
 </template>
@@ -60,12 +71,13 @@ defineProps<IProps>()
     }
   }
 
-  .search-recommend{
+  .search-recommend {
     display: flex;
     padding: 0 10px 8px;
-    .tag{
+
+    .tag {
       font-size: 12px;
-      background: rgb(242,242,242,0.3);
+      background: rgb(242, 242, 242, 0.3);
       border-radius: 10px;
       padding: 2px 8px;
       margin-right: 10px;
@@ -75,9 +87,9 @@ defineProps<IProps>()
 </style>
 
 <style lang="scss">
- .home-top{
-   .van-field__right-icon{
+.home-top {
+  .van-field__right-icon {
     margin-right: 0;
-   }
- }
+  }
+}
 </style>
